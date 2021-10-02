37db8f72a49e1a56bd1bb0449759245b7dc5d4cd
Lilli Freischem <ljfreischem@gmail.com>
2021-10-02 17:11:24 +0100

    TypeScript to JavaScript

--- sunny-days/src/pages/Tab3.js
+++ sunny-days/src/pages/Tab3.js
@@ -2,7 +2,7 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
-const Tab3: React.FC = () => {
+const Tab3 = () => {
   //temporalRes = "daily" or "hourly" or "monthly"
   //latitude = string from -90 to 90 with 4 decimal places
   //longitude = string from -180 to 180 with 4 decimal places
