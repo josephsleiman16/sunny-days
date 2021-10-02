fb25ee67563c52218c2a428ec7cad1a13089ca47
kayzee805 <kayzee805@live.com>
2021-10-02 23:19:04 +0100

    made res graph just monthly

--- sunny-days/src/components/graph.js
+++ sunny-days/src/components/graph.js
@@ -6,29 +6,58 @@ import { Line } from 'react-chartjs-2';
 const Graph  = ({data}) => {
 
     if(!data){return (<IonPage></IonPage>)};
-  const timeLineData = data?.properties?.parameter?.ALLSKY_SFC_SW_DWN;
+    const timeLineData = data?.properties?.parameter?.ALLSKY_SFC_SW_DWN;
 
-  const labels = Object.keys(timeLineData);
+    const labels = Object.keys(timeLineData);
+    //daily YYYYMMDD = 8
+    //HOURLY YYYYMMDDHH = 10
+    //Monthly YYYYmm = 6
 
-  const years = labels.map(x => x.substring(0,4));
-  const months = labels.map(x => parseInt(x.substring(5,6)));
+    const monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+                        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
 
-  var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", 
-                     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+    let theLabels= '';
+    console.log('labesl: ',labels);
 
-  const theseMonths = months.map(x => monthNames[x]);
+    const years = labels.map(x => x.substring(0,4));
+    const months = labels.map(x => parseInt(x.substring(4,6)));
+    const theseMonths = months.map(x => monthNames[x-1]);
+    if (labels[0].length===8){
+        //daily
+        const days = labels.map(x => parseInt(x.substring(6,8)));
+        // date month year 
+        theLabels = theseMonths.map((x, i) => days[i]+' '+x + ' ' + years[i]);
 
-  let theLabels = theseMonths.map((x, i) => x + ' ' + years[i]);
+    } else if(labels[0].length===10){
+        //Hourly
+        const days = labels.map(x => parseInt(x.substring(6,8)));
+        const hours = labels.map( x=> parseInt(x.substring(8,10)))         
+        const hoursTrue = hours.map( x=> x>=12 ? (x==12? x+'pm': (x-12)+'pm') : (x==0 ? x+12+'am': x+'am'));    
+        theLabels = theseMonths.map((x, i) =>  hoursTrue[i]+' '+ days[i]+' '+x + ' ' + years[i]);
+     
+    }else{
+        //monthly
+        let filterLabels = months.filter(x => x<13);
+        let filterYears = years.filter((x,i) => months[i]<13);
+        console.log('test ',filterLabels);
+        theLabels = filterLabels.map((x,i) => monthNames[x-1] +' '+filterYears[i]);
 
-  const values = Object.values(timeLineData);
+        // [11,12,13,01,02]  [19,19,19,20,20]
+        //[11,12,01,02]  [19,19,19,20]
+   //     theLabels = theseMonths.map((x, i) =>  x + ' ' + years[i]);
 
-  console.log(theLabels);
-  console.log(values);
+    }
 
-  const state = {
+
+    const values = Object.values(timeLineData);
+
+    console.log(theLabels);
+    console.log(values);
+
+    const state = {
     labels: theLabels,
     datasets: [ 
-      {
+        {
         label: 'Sunshine',
         fill: false,
         lineTension: 0.5,
@@ -37,9 +66,9 @@ const Graph  = ({data}) => {
         borderWidth: 1,
         pointRadius: 1,
         data: values
-      }
+        }
     ]
-  }
+    }
 
 
   return (
@@ -52,21 +81,40 @@ const Graph  = ({data}) => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Results</IonTitle>
           </IonToolbar>
         </IonHeader>
         <Line
           data={state}
           options={{
-            // title:{
-            //   display:true,
-            //   text:'Average Rainfall per month',
-            //   fontSize:20
-            // },
-            // legend:{
-            //   display:true,
-            //   position:'right'
-            // }
+            responsive: true,
+            title:{
+              display:true,
+              text:'Average Rainfall per month',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'right'
+            },
+            scales: {
+                y: {
+                    display:true,
+                    title:{
+                        display:true,
+                        text:"y axis",
+                        color: "black"
+                    }
+                },
+                x: {
+                    display:true,
+                    title:{
+                        display:true,
+                        text:"x axis",
+                        color: "black"
+                    }
+                }
+            }
           }}
         />
         {/* <ExploreContainer name="Tab 2 page" /> */}
