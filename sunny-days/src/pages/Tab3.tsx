7e990b387a884a7b5fcddc2bdc01499c6bde6e0e
Joseph Sleiman <s1766037@sms.ed.ac.uk>
2021-10-02 12:08:54 +0100

    Joe's Logic

--- sunny-days/src/pages/Tab3.tsx
+++ sunny-days/src/pages/Tab3.tsx
@@ -3,6 +3,23 @@ import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
+  //temporalRes = "daily" or "hourly" or "monthly"
+  //latitude = string from -90 to 90 with 4 decimal places
+  //longitude = string from -180 to 180 with 4 decimal places
+  //startDate = string format: YYYYMMDD
+  //endDate = string format: YYYYMMDD
+  let apiUrl = 'https://power.larc.nasa.gov/api/temporal/' + temporalRes + '/point?parameters=ALLSKY_SFC_SW_DWN&community=RE&longitude=' + longitude + '&latitude=' + latitude + '&start=' + startDate + '&end=' + endDate + '&format=JSON';
+  let json = fetch(apiUrl);
+  let obj = JSON.parse(json);
+
+  let solarArray = []; //NOTE: units of solar irradiance are kW-hr/m^2/day
+  let dateArray = [];
+  for val in obj.properties.parameter.ALLSKY_SFC_SW_DWN:
+    dateArray.append(val)
+
+  for val in obj.properties.parameter.ALLSKY_SFC_SW_DWN.date:
+    solarArray.append(val)
+
   return (
     <IonPage>
       <IonHeader>
